c6b4389c52b452c9c2cb2335e55e864df3b14929
Jeremy Edberg <jedberg@reddit.com>
2010-05-17 12:40:41 -0700

    * Add services and scripts to public repos for ease of reddit installation  * bring discount up to date  * fix consistency errors in the self-serve traffic page

--- scripts/log_q.py
+++ scripts/log_q.py
@@ -61,6 +61,7 @@ def run(limit=100, streamfile=None, verbose=False):
         pretty_lines = []
 
         make_lock_seen = False
+        flaky_db_seen = False
 
         for tpl in d['traceback']:
             tb.append(tpl)
@@ -70,6 +71,8 @@ def run(limit=100, streamfile=None, verbose=False):
             elif (text.startswith("with g.make_lock(") or
                   text.startswith("with make_lock(")):
                 make_lock_seen = True
+            elif (text.startswith("(ProgrammingError) server closed the connection")):
+                flaky_db_seen = True
             key_material += "%s %s " % (filename, funcname)
             pretty_lines.append ("%s:%s: %s()" % (filename, lineno, funcname))
             pretty_lines.append ("    %s" % text)
@@ -80,6 +83,23 @@ def run(limit=100, streamfile=None, verbose=False):
             fingerprint = "memcache_suckitude"
         elif exc_type == "TimeoutExpired" and make_lock_seen:
             fingerprint = "make_lock_timeout"
+        elif exc_desc.startswith("(OperationalError) FATAL: the database " +
+                                 "system is in recovery mode"):
+            fingerprint = "recovering_db"
+        elif exc_desc.startswith("(OperationalError) could not connect " +
+                                 "to server"):
+            fingerprint = "unconnectable_db"
+        elif exc_desc.startswith("(OperationalError) server closed the " +
+                                 "connection unexpectedly"):
+            fingerprint = "flaky_db_op"
+        elif exc_type == "ProgrammingError" and flaky_db_seen:
+            fingerprint = "flaky_db_prog"
+            # SQLAlchemy includes the entire query in the exception
+            # description which can sometimes be gigantic, in the case of
+            # SELECTs. Get rid of it.
+            select_pos = exc_str.find("SELECT")
+            if select_pos > 0:
+                exc_str = exc_str[pos]
         elif exc_type == "NoServerAvailable":
             fingerprint = "cassandra_suckitude"
         else:
